ff957dcddd9d16892b71bfd9694fa7e73fba34ae
aserahmad <53275422+aserahmad@users.noreply.github.com>
2019-08-05 20:36:09 -0500

    Uploading the Maze game files.

---
+++ Lab10/maze.js
@@ -0,0 +1,43 @@
+"use strict";
+let gameStarted = false;
+
+$(document).ready(function() {
+
+    /**
+     * what happens when the start button is clicked (see the line comments).
+     */
+    $('#start').click(function() {
+        gameStarted = true; // set game start.
+        $('#status').text("Try to go to the \"E\" without touching the boundaries."); // reset game title.
+        $('#maze div.boundary').removeClass('youlose'); // reset boundary.
+        $('#maze div.boundary').on('mouseenter', lose); // attach mouse event to boundary.
+        $('#maze').on('mouseleave', lose); // attach mouse event to rest of the body.
+    });
+
+    /**
+     * what happens when the user clicks the end button (see each line comment).
+     */
+    $('#end').click(function() {
+        if (!gameStarted) { // account for clicking when no game is being played or has ended.
+        $('#status').text("Click the \"S\" to begin."); // reset game title.
+    } else { // if game has started but not ended..
+        gameStarted = false; // reset game start flag.
+        alert("You win!"); // announce winning by alert message.
+        $('#status').text("You win! Click the \"S\" to do it again."); // announce winning by alert message.
+    }
+        $('#maze div.boundary').removeClass('youlose'); // reset boundary.
+        $('#maze, #maze div.boundary').off(); // detach mouse event.
+    });
+});
+
+/**
+ * executed when the player loses. see line comments for details.
+ */
+function lose() {
+    if (gameStarted) { // only do the following if game started to avoid annoying the user.
+        $('#maze div').addClass('youlose'); // attach lose class to the boundaries to turn them to red.
+        alert('Sorry! You lose :('); // announce loss by alert message.
+        $('#status').text("You lose!"); // announce loss by text.
+    }
+    gameStarted = false;
+}
